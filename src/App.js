934bc64a42dbca8c38414f99d41f94b13cb826e2
frederikhendrix <rikhendrix@live.nl>
2023-03-20 13:04:01 +0100

    PWA done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
-function App() {
+const App = () => {
+  const [testSite, setTestSite] = useState("");
+
+  useEffect(() => {
+    setTestSite("Sheesh");
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Nice</h1>
+      <p>{testSite}</p>
     </div>
   );
-}
+};
 
 export default App;
