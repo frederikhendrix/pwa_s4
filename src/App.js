d33fe125efa2b4cf3ad5ed4da1d61aead8d055b1
frederikhendrix <rikhendrix@live.nl>
2023-03-29 11:52:04 +0200

    first version

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from "./logo.svg";
 import "./App.css";
 import axios from "axios";
+import { useState } from "react";
+
 function App() {
-  const [color, setColor] = useState("ffffff");
-  function handleColorChange(color) {
-    setColor(color);
-    axios.put(
-      `http://192.168.0.100/api/7KwyaQsT91WJE7aXrXwCzFf7D-PYItXQ4Zk5Merw/lights/EE0BA2/state`,
-      {
-        on: true,
-        sat: 254,
-        bri: 150,
-        hue: parseInt(color, 16),
-      }
-    );
+  const [color, setColor] = useState();
+
+  function handleColorChange(color2) {
+    setColor(color2);
+    try {
+      axios
+        .put(
+          `http://192.168.0.100/api/7KwyaQsT91WJE7aXrXwCzFf7D-PYItXQ4Zk5Merw/lights/1/state`,
+          {
+            on: true,
+            sat: 254,
+            bri: 150,
+            hue: color2,
+          }
+        )
+        .then((result) => console.log(result));
+    } catch (error) {
+      console.log(error);
+    }
   }
   return (
     <div className="App">
-      <button onClick={() => handleColorChange("ff0000")}>Red</button>
-      <button onClick={() => handleColorChange("00ff00")}>Green</button>
+      <button onClick={() => handleColorChange(0)}>Red</button>
+      <button onClick={() => handleColorChange(25500)}>Green</button>
+      <button onClick={() => handleColorChange(46920)}>Blue</button>
+      <div>{color}</div>
     </div>
   );
 }
